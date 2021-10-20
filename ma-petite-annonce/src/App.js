91611f2200af8230bc3d8bbad7d658065543cd0c
Ulysse Perret <ulysseperret@orange.fr>
2021-10-20 13:56:33 +0200

    Gestion meilleur

--- ma-petite-annonce/src/App.js
+++ ma-petite-annonce/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './styles/img/logo.svg';
+import './styles/App.css';
+import './styles/index.css';
 
 function App() {
   return (
