85ad0b2b769f263fe9da8de11c3d04d4852f7d81
Ulysse Perret <ulysseperret@orange.fr>
2021-10-22 09:42:44 +0200

    Ajout pages

--- ma-petite-annonce/src/App.js
+++ ma-petite-annonce/src/App.js
@@ -1,24 +1,31 @@
 import './styles/sass/App.scss';
+
 import Header from './components/Header';
-import CreateAnnonce from './components/CreateAnnonce';
 import Footer from './components/Footer';
-import Homepage from './pages/Homepage';
 import Menu from './components/Menu';
-import OneAnnonce from './components/OneAnnonce';
 import PopupContact from './components/PopupContact';
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import OneAnnonce from './pages/OneAnnonce';
+import Homepage from './pages/Homepage';
+import CreateAnnonce from './pages/CreateAnnonce';
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/homemepage" component={Homepage} />
-        <Route path="/createAnnonce" component={CreateAnnonce} />
-        <Route path="/OneAnnonce" component={OneAnnonce} />
-        <Route path="/PopupContact" component={PopupContact} />
-        <Route path="/Menu" component={Menu} />
-        <Route path="/Header" component={Header} />
-        <Route path="/Footer" component={Footer} />
+        <Route path="/" component={Homepage} />
+        <Route path="/createAnnonce" exact component={CreateAnnonce} />
+        <Route path="/oneAnnonce" exact component={OneAnnonce} />
+        <Route path="/popupContact" exact component={PopupContact} />
+        <Route path="/menu" exact component={Menu} />
+        <Route path="/header" exact component={Header} />
+        <Route path="/footer" exact component={Footer} />
+        <Route path="/a-propos" exact component={About} />
+        <Route component={NotFound} />
 
       </Switch>
     </BrowserRouter>
