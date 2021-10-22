c066c0b7c35cda247beceee058d457f482a50138
Ulysse Perret <ulysseperret@orange.fr>
2021-10-22 14:42:46 +0200

    mise a jour aprem

--- ma-petite-annonce/src/App.js
+++ ma-petite-annonce/src/App.js
@@ -1,20 +1,20 @@
 //Base - Reacte
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import { BrowserRouter, Switch, Route } from "react-router-dom";
 //style
 import './styles/sass/App.scss';
-//compoenent
+//component
 import Header from './components/Header';
 import CreateAnnonce from './components/CreateAnnonce';
 import Footer from './components/Footer';
-import Homepage from './components/Homepage';
+//import Homepage from './components/Homepage';
 import Menu from './components/Menu';
 import OneAnnonce from './components/OneAnnonce';
 import PopupContact from './components/PopupContact';
 //Pages
 import Homepage from './pages/Homepage';
-import About from "./pages/About";
-import NotFound from "./pages/NotFound";
+//import About from "./pages/About";
+//import NotFound from "./pages/NotFound";
 //import OneAnnonce from './pages/OneAnnonce';
 //import CreateAnnonce from './pages/CreateAnnonce';
 function App() {
@@ -30,7 +30,7 @@ function App() {
     </div>
 
     /**
-     *    <BrowserRouter>
+     * <BrowserRouter>
       <Switch>
         <Route path="/" component={Homepage} />
         <Route path="/a-propos" exact component={About} />
--- ma-petite-annonce/src/components/Header.js
+++ ma-petite-annonce/src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../styles/sass/header.scss';
-import Logo from '../images/logo.svg'
+//import Logo from '../images/logo.svg'
 
 const Header = () => {
   return (
-    <section className="header">
+    <header className="header">
       <div>
+
         <h1 className="logo">Ma Petite Annonce</h1>
       </div>
       <div className="btn">
+
         <a href="/CreateAnnonce">Créer une annonce</a>
+
       </div>
-    </section >
+    </header >
   );
 };
 
