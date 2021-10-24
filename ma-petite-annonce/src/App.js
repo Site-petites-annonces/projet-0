8da6d3c6f7e3528bcb72b8c435d0fbd33a23a214
Ulysse Perret <ulysseperret@orange.fr>
2021-10-24 15:54:17 +0200

    Verif

--- ma-petite-annonce/src/App.js
+++ ma-petite-annonce/src/App.js
@@ -1,24 +1,24 @@
 //Base - Reacte
 import React from "react";
-//import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 //style
 import './styles/sass/App.scss';
 //component
 import Header from './components/Header';
 import CreateAnnonce from './components/CreateAnnonce';
 import Footer from './components/Footer';
-//import Homepage from './components/Homepage';
 import Menu from './components/Menu';
 import OneAnnonce from './components/OneAnnonce';
 import PopupContact from './components/PopupContact';
 //Pages
 import Homepage from './pages/Homepage';
-//import About from "./pages/About";
-//import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 //import OneAnnonce from './pages/OneAnnonce';
 //import CreateAnnonce from './pages/CreateAnnonce';
 function App() {
   return (
+    /** */
     <div>
       <Header />
       <Menu />
@@ -27,17 +27,18 @@ function App() {
       <Homepage />
       <OneAnnonce />
       <PopupContact />
+
     </div>
 
-    /**
-     * <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Homepage} />
-        <Route path="/a-propos" exact component={About} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter> 
-     */
+    /** 
+       <BrowserRouter>
+         <Switch>
+           <Route path="/" component={Homepage} />
+           <Route path="/a-propos" exact component={About} />
+           <Route component={NotFound} />
+         </Switch>
+       </BrowserRouter>
+       */
   );
 }
 
--- ma-petite-annonce/src/pages/About.js
+++ ma-petite-annonce/src/pages/About.js
@@ -6,9 +6,10 @@ const About = () => {
             <h1>A propos</h1>
             <br />
             <p>Site de petite annonces crée par Thierno Balde, Nikola Marinkovi, Baptis dantas et Ulysse Perret.
-            Le site porposse des annonces pour acheter ou vendres des produits entre particulier
+                Le site porposse des annonces pour acheter ou vendres des produits entre particuliers.
 
-        </p></div>
+            </p>
+        </div>
     );
 };
 
