d2e5480ae586a3dd4251c81de39a327da9bae821
Ulysse Perret <ulysseperret@orange.fr>
2021-11-09 10:56:11 +0100

    mise a jour navigationh

---
+++ ma-petite-annonce/src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navlink } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <div className="navigation">
+            <Navlink exact to="/" activeClassName="nav-active" >
+                Acceuil
+            </Navlink>
+            <Navlink exact to="a-propos" activeClassName="nav-active">
+                À propos
+            </Navlink>
+        </div>
+    );
+};
+
+export default Navigation;
