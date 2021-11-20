a71f78e41e9ef60fdb599a789ed8e622e1b9f4f9
Ulysse Perret <ulysseperret@orange.fr>
2021-11-20 10:24:46 +0100

    about et node

--- ma-petite-annonce/src/pages/About.js
+++ ma-petite-annonce/src/pages/About.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Navigation from "../components/Navigation"
 
 const About = () => {
+
     return (
         <div>
+
             <Navigation />
             <h1>A propos</h1>
             <br />
