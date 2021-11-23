f2116ffdc09ae7c16e4236bf887f1f1bf4bccad7
Ulysse Perret <ulysseperret@orange.fr>
2021-11-23 21:00:18 +0100

    logo/ clarification

--- ma-petite-annonce/src/components/Header.js
+++ ma-petite-annonce/src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../styles/sass/header.scss';
 //import Logo from '../images/logo.svg'
-
+//
 const Header = () => {
   return (
+    // <figcaption>Le Logo du site Ma Petite annonce</figcaption>
+
     <header className="header">
       <div>
+
         <figure>
-          <image src="./images/logotest.svg" width="100"> </image>
-          <figcaption>Le Logo du site</figcaption>
+          <img src="./img/logotest.svg" alt="logo"></img>
         </figure>
         <h1 className="logo">Ma Petite Annonce</h1>
+
 
       </div>
       <div className="btn">
--- ma-petite-annonce/src/index.js
+++ ma-petite-annonce/src/index.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Header from './components/Header';
+import CreateAnnonce from './components/CreateAnnonce';
+import Footer from './components/Footer';
+import Menu from './components/Menu';
+import OneAnnonce from './components/OneAnnonce';
+import PopupContact from './components/PopupContact';
+import Homepage from './pages/Homepage';
+import About from "./pages/About";
 import './styles/css/index.css';
-import App from './App'
-/**
-function App() {
-  return (
-    <div class="App">
-      <h1>Hello petite annonces</h1>
-    </div>
-  )
+//import App from './App'
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <div clasName="App">
+        <Header />
+        <Homepage />
+
+      </div>
+
+      <h1>Liste des annonces actuelle</h1>
+      <ul>
+        <li>Vélos</li>
+        <li> Jeux</li>
+        <li><a href="/OneAnnonce">Informatique</a></li>
+      </ul>
+
+      <Footer />
+    </div >
+  }
 }
- */
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement)
-
-
+/**
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'));
- /***/
+<React.StrictMode>
+ <App />
+</React.StrictMode>,
+document.getElementById('root'));
+*/
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement)
