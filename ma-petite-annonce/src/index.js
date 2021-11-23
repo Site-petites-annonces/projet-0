d19f59b2b51f0b9197dabc27ad665ad79fb52ff0
Ulysse Perret <ulysseperret@orange.fr>
2021-11-23 20:42:45 +0100

    mise a jour App

--- ma-petite-annonce/src/index.js
+++ ma-petite-annonce/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/css/index.css';
-
+import App from './App'
+/**
 function App() {
   return (
     <div class="App">
@@ -10,15 +11,15 @@ function App() {
     </div>
   )
 }
-
+ */
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement)
 
 
-/**
- * ReactDOM.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'));
- */
+ /***/
